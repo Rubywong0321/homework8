12b03428b6be0ee786b425a037af607fde7406bc
Rubywong0321 <37624222+Rubywong0321@users.noreply.github.com>
2019-10-16 17:10:04 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-    var $li = $('.four>section>div:nth-of-type(2) div a');
+    var $li = $('ul.tab-title li');
         $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
     
         $li.click(function(){
